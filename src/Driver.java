45e74988653245e0b45fee3f52639c487fc8251f
Daniel Wei <dominum15@outlook.com>
2019-06-17 17:52:31 -0500

    Calculate WVC

--- src/Driver.java
+++ src/Driver.java
@@ -1,8 +1,6 @@
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Scanner;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Driver {
     public static void main(String[] args) throws IOException {
@@ -23,7 +21,29 @@ public class Driver {
                 } else map.put(token, 1);
             }
         }
-        System.out.println(map.size());
+
+        int numOfDistinctWords = map.size();
+
+        int[] top = new int[7];
+        for (int i = 0; i < top.length; i++) {
+            Iterator<Map.Entry<String, Integer>> iterator = map.entrySet().iterator();
+            Map.Entry best = null;
+            while (iterator.hasNext()) {
+                Map.Entry next = iterator.next();
+                if ((Integer)next.getValue() > top[i]) {
+                    top[i] = (Integer) next.getValue();
+                    best = next;
+                }
+            }
+            map.remove(best.getKey(), best.getValue());
+        }
+        int sum = 0;
+        for (int i : top)
+            sum+=i;
+        double avg = sum/(top.length + 0.0);
+
+        double wvc = numOfDistinctWords/avg;
+        System.out.println(wvc);
     }
     private static String removePunct(String in, String[] punctuation) {
         for (String s : punctuation) {
